79c4e299e673954209be464b56b8b35871cd41e5
Mauro Zac <tapirus.terrestris@gmail.com>
2020-04-08 11:09:22 -0300

    Update compara.py

--- compara.py
+++ compara.py
@@ -6,7 +6,7 @@ Modelagem em tempo real | COVID-19 no Brasil
 """
 
 import pandas as pd
-
+import requests
 
 __author__ = "Mauro Zackiewicz"
 __copyright__ = "Copyright 2020"
@@ -18,9 +18,19 @@ __status__ = "Experimento"
 
 # 1 - preparacao dos dados
 
-# raw = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/total_deaths.csv").fillna(0.0)
 raw = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/new_deaths.csv").fillna(0.0)
+# raw = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/total_deaths.csv").fillna(0.0)
 raw = raw.drop(columns='date')
+popu = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/locations.csv").set_index('countriesAndTerritories')
+
+
+# dados do SEADE => CSV mal formado, url sem ceritifcado valido, sem serie historica
+# oh shit, so para dar mais trabalho
+# 
+# seade = "http://www.seade.gov.br/wp-content/uploads/2020/04/Dados-covid-19-Est-SP.csv"
+# sp = pd.read_csv(requests.get(seade, verify=False).text, sep=';', decimal=',')
+# a = requests.get(seade, verify=False).text
+# sp = pd.read_csv(a, sep=';', decimal=',', lineterminator="\r\n")
 
 # ancorar todos no primeiro dia com 3 mortes => no tamanho do Brasil
 # pq usar numero baixo introduz muito ruido
@@ -29,12 +39,15 @@ inicio = raw.ge(3).idxmax()
 br = raw['Brazil'][inicio['Brazil']:]  # Series
 br_n = br.shape[0]
 
-br = pd.DataFrame({'Brazil':br})
+data = pd.DataFrame({'Brazil':br})
+data['Brazil'] = data['Brazil'] * (10**6) / popu['population']['Brazil']
 
 for k in inicio.keys():
     if k == "Brazil": continue
+    if k not in popu.index: continue
     if inicio[k] == 0 or inicio[k] > inicio["Brazil"]: continue
     C = raw[k][inicio[k]:inicio[k]+br_n]
-    br[k] = C.values
+    data[k] = C.values * (10**6) / popu['population'][k]
 
-br.corr()
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.corr.html
+data.corr()
